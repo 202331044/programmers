e6599e6180f6109efd56d05978f505bfd3c33078
AJY <skyscraper2936@gmail.com>
2026-04-19 12:27:12 +0900

    solve: 두 수의 나눗셈 (level0)

---
+++ level0/divide_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+
+    //int answer = static_cast<double>(num1) / num2 * 1000;
+    //return answer;
+
+    return num1 * 1000 / num2;
+}
