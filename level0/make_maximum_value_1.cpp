af4a9adbb784d6a6e6604b6872ebce4590da71da
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-20 15:45:22 +0900

    solve: 최댓값 만들기(1) (level0)

---
+++ level0/make_maximum_value_1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end(), greater<int>());
+    
+    int answer = numbers[0] * numbers[1];
+    
+    return answer;
+}
