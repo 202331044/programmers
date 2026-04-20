62f288c94769b4698fee33c9786dfa7ffc2fb268
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-20 16:38:28 +0900

    solve: 제곱수 판별하기 (level0)

---
+++ level0/check_perfect_square.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int num = sqrt(n);
+    
+    if(num * num == n) return 1;
+    return 2;
+}
