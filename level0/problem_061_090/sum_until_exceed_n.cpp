91bb495d7cf96832a575912806d87ea27360edb5
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-25 15:12:39 +0900

    solve: n보다 커질 때까지 더하기 (level0)

---
+++ level0/problem_061_090/sum_until_exceed_n.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int n) {
+    int sum = 0;
+    
+    for(int num: numbers)
+    {
+        sum += num;
+        if(sum > n) return sum;
+    }
+}
