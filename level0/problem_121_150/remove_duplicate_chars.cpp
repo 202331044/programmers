a4388e23faf14e235cc3bb62f1a9b57ac0839a45
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-29 11:35:15 +0900

    solve: 중복된 문자 제거 (level0)

---
+++ level0/problem_121_150/remove_duplicate_chars.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer;
+    
+    for(char ch: my_string)
+    {
+        if(answer.find(ch) == string::npos)
+            answer += ch;
+    }
+    
+    return answer;
+}
