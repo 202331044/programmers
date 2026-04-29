c25d6620bda3f3bc903c14859d4878461dc9a2e6
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-29 11:05:55 +0900

    solve: 배열 회전시키기 (level0)

---
+++ level0/problem_121_150/array_rotation.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, string direction) {
+    vector<int> answer;
+    
+    if(direction == "right")
+    {
+        answer.push_back(numbers.back());
+        answer.insert(answer.end(), numbers.begin(), numbers.end() - 1);
+    }
+    else
+    {
+        answer.insert(answer.end(), numbers.begin() + 1, numbers.end());
+        answer.push_back(numbers.front());
+    }
+    
+    return answer;
+}
