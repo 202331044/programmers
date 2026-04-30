2d8788bd2d6d019cfc9095b815b01ce3ab1a7667
202331044 <131847052+202331044@users.noreply.github.com>
2026-04-30 22:00:20 +0900

    solve: 배열 만들기 5 (level0)

---
+++ level0/problem_151_180/make_array_5.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> intStrs, int k, int s, int l) {
+    vector<int> answer;
+    
+    for(string& str: intStrs)
+    {
+        int num = stoi(str.substr(s, l));
+        if(num > k) answer.push_back(num);
+    }
+    
+    return answer;
+}
