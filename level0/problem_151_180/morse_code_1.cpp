ca80497f1f4244e0736f0d9418727a5017818ee6
202331044 <131847052+202331044@users.noreply.github.com>
2026-05-01 11:07:48 +0900

    solve: 모스부호 (1) (level0)

---
+++ level0/problem_151_180/morse_code_1.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(string letter) {
+    unordered_map<string, char> morse = { 
+    {".-", 'a'}, {"-...", 'b'}, {"-.-.", 'c'}, 
+    {"-..", 'd'}, {".", 'e'}, {"..-.", 'f'},
+    {"--.", 'g'},{"....", 'h'} ,{"..", 'i'}, 
+    {".---", 'j'}, {"-.-", 'k'}, {".-..", 'l'},
+    {"--", 'm'}, {"-.", 'n'}, {"---", 'o'},
+    {".--.", 'p'}, {"--.-", 'q'}, {".-.", 'r'},
+    {"...", 's'}, {"-", 't'}, {"..-", 'u'},
+    {"...-", 'v'}, {".--", 'w'}, {"-..-", 'x'}, 
+    {"-.--", 'y'}, {"--..", 'z'}};
+   
+    string answer, str;
+    istringstream iss(letter);
+
+    while(iss >> str)
+        answer += morse[str];
+    
+    return answer;
+}
