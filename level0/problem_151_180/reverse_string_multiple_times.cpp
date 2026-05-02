08cdd5e15f5875f0cc29c3e43852e90e0508ea45
202331044 <131847052+202331044@users.noreply.github.com>
2026-05-02 12:35:10 +0900

    solve: 문자열 여러 번 뒤집기 (level0)

---
+++ level0/problem_151_180/reverse_string_multiple_times.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string, vector<vector<int>> queries) {
+    for(auto query: queries)
+    {
+        int s = query[0], e = query[1];
+        
+        reverse(my_string.begin() + s, my_string.begin() + e + 1);
+    }
+    
+    return my_string;
+}
