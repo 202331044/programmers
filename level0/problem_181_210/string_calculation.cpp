4f487ff6ace6f145e9787b67b67f6c3773a4072e
AJY <skyscraper2936@gmail.com>
2026-05-03 12:33:56 +0900

    refactor: simplify solution in string_calculation.cpp

--- level0/problem_181_210/string_calculation.cpp
+++ level0/problem_181_210/string_calculation.cpp
@@ -6,33 +6,32 @@ using namespace std;
 
 int solution(string my_string) {
     int total = 0;
-    
+
     istringstream iss(my_string);
-//     string str;
-//     char op = '+';
-    
-//     while(iss >> str)
-//     {
-//         if(str == "+") op = '+';
-//         else if(str == "-") op = '-';
-//         else
-//         {
-//             int num = stoi(str);
-//             if(op == '-') total -= num;
-//             else total += num;
-//         }
-//     }
-    
+    //     string str;
+    //     char op = '+';
+
+    //     while(iss >> str)
+    //     {
+    //         if(str == "+") op = '+';
+    //         else if(str == "-") op = '-';
+    //         else
+    //         {
+    //             int num = stoi(str);
+    //             if(op == '-') total -= num;
+    //             else total += num;
+    //         }
+    //     }
+
     iss >> total;
-    
-    while(!iss.eof())
+
+    char op;
+    int num;
+    while (iss >> op >> num)
     {
-        char op;
-        int num;
-        iss >> op >> num;
-        if(op == '+') total += num;
+        if (op == '+') total += num;
         else total -= num;
     }
-    
+
     return total;
 }
