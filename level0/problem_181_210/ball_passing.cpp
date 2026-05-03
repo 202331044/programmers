81eff77cd028d72a079daa0efa8073a944d604ae
AJY <skyscraper2936@gmail.com>
2026-05-03 12:31:26 +0900

    feat: add first solution to ball_passing.cpp

--- level0/problem_181_210/ball_passing.cpp
+++ level0/problem_181_210/ball_passing.cpp
@@ -4,6 +4,21 @@
 using namespace std;
 
 int solution(vector<int> numbers, int k) {
-    int idx = 2 * (k-1) % numbers.size();
-    return numbers[idx];
+    //vector<int> order;
+    //int n = numbers.size();
+
+    //for (int i = 0; i < n; i += 2)
+    //    order.push_back(numbers[i]);
+
+    //if (n % 2 != 0)
+    //{
+    //    for (int j = 1; j < n; j += 2)
+    //        order.push_back(numbers[j]);
+    //}
+
+    //if (order.size() >= k) return order[k - 1];
+    //return order[(k - 1) % order.size()];
+
+     int idx = 2 * (k-1) % numbers.size();
+     return numbers[idx];
 }
