5a16f1c09ad3f38ee4a8350a7d685dc8fe9671c6
202331044 <131847052+202331044@users.noreply.github.com>
2026-05-10 11:42:06 +0900

    solve: 배열 만들기 2 (level0)

---
+++ level0/problem_211_240/make_array_2.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int l, int r) {
+    vector<int> answer;
+
+    for(int i = 5; i <= r; i += 5)
+    {
+        string str = to_string(i);
+        bool valid = true;
+        
+        for(char ch: str)
+        {
+            if(ch != '0' && ch != '5')
+            {
+                valid = false;
+                break;
+            }
+        }
+        
+        if(valid && i >= l) answer.push_back(i);
+    }
+    
+    if(answer.empty()) return {-1};
+    return answer;
+}
