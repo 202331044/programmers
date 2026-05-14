e75ca6a497b206662794e49d5c46cab9e8fd94e3
202331044 <131847052+202331044@users.noreply.github.com>
2026-05-14 12:02:16 +0900

    solve: 문자열을 정수로 바꾸기 (level1)

---
+++ level1/problem_001_030/convert_string_to_int.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int solution(string s) {
+    
+//     istringstream iss(s);
+    
+//     int num;
+//     iss >> num;
+    
+//     return num;
+    
+    return stoi(s);
+}
