33b1d9bae0fdbd512d2a9b4703f9f373a886e4d3
202331044 <131847052+202331044@users.noreply.github.com>
2026-05-15 11:01:01 +0900

    solve: 평균 구하기 (level1)

---
+++ level1/problem_001_030/calculate_average.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    int sum = 0;
+    
+    for(int num: arr)
+        sum += num;
+    
+    return static_cast<double>(sum) / arr.size();
+}
