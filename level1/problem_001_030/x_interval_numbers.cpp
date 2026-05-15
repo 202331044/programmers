016d6846d2b5ff769a6f7968687dfc0af2846b81
202331044 <131847052+202331044@users.noreply.github.com>
2026-05-15 11:07:38 +0900

    solve: x만큼 간격이 있는 n개의 숫자 (level1)

---
+++ level1/problem_001_030/x_interval_numbers.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    
+    // long long num = x;
+    // for(int i = 0; i < n; ++i)
+    // {
+    //     answer.push_back(num);
+    //     num += x;
+    // }
+    
+    for(int i = 1; i <= n; ++i)
+        answer.push_back(x * i);
+
+    return answer;
+}
