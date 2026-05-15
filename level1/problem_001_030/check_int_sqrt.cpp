765ebc62d5699ebee494303a8604af4679819a8b
202331044 <131847052+202331044@users.noreply.github.com>
2026-05-15 10:56:24 +0900

    solve: 정수 제곱근 판별 (level1)

---
+++ level1/problem_001_030/check_int_sqrt.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long x = sqrt(n);
+    return x * x == n ? (x + 1) * (x + 1) : -1;
+}
