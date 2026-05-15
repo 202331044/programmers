3c2c5726564539134899cb2dddd9fb39f0572ee0
202331044 <131847052+202331044@users.noreply.github.com>
2026-05-15 11:18:13 +0900

    solve: 문자열 내 p와 y의 개수 (level1)

---
+++ level1/problem_001_030/count_p_y.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    int p = 0, y = 0;
+    
+    for(char& ch: s)
+    {
+        if(ch == 'p' || ch == 'P') p++;
+        else if(ch == 'y' || ch == 'Y') y++;
+    }
+
+    return p == y;
+}
