3c590f16b94c7c6fb39a05323dbaf2e3d5149daa
202331044 <131847052+202331044@users.noreply.github.com>
2026-06-09 12:46:49 +0900

    solve: N개의 최소공배수 (level2)

---
+++ level2/problem_001_030/n_lcm.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> arr) {
+    int answer = 1;
+    
+    for(int num: arr)
+        answer = lcm(answer, num);
+    
+    return answer;
+}
