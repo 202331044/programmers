a5c3b8770327f5c81d1fbcf5a380496ba7bc5183
202331044 <131847052+202331044@users.noreply.github.com>
2026-06-19 13:39:38 +0900

    solve: k진수에서 소수 개수 구하기 (level2)

---
+++ level2/problem_031_060/prime_count_k_base.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string make_kth(int n, int k)
+{
+    string str;
+    
+    while(n)
+    {
+        str += (n % k) + '0';
+        n /= k;
+    }
+    
+    reverse(str.begin(), str.end());
+    
+    return str;
+}
+
+bool isPrime(long long num)
+{   
+    if (num < 2) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
+    
+    for(long long i = 3; i * i <= num; ++i)
+        if(num % i == 0) return false;
+
+    return true;
+}
+
+int solution(int n, int k) {
+    string str = make_kth(n, k);
+    string number;
+    int answer = 0;
+    
+    for(char ch: str)
+    {
+        if(ch == '0')
+        {
+            
+            if(!number.empty() && isPrime(stoll(number)))
+                answer++;
+            
+            number.clear();
+        }
+        else
+            number += ch;
+    }
+    
+    if(!number.empty() && isPrime(stoll(number)))
+        answer++;
+    
+    return answer;
+}
